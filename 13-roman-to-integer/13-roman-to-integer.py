1cf808c6961bb08e9e60219674dc3e6c7f6e098a
Samridh Anand <89855495+samridh3215@users.noreply.github.com>
2022-09-05 07:17:36 +0530

    Time: 38 ms (92.89%), Space: 13.4 MB (81.99%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        maps={"I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000}
+        result =0
+        for i in range(len(s)-1):
+            if(maps[s[i]]<maps[s[i+1]]):
+                result -= maps[s[i]]
+            else:
+                result+= maps[s[i]]
+        return (result+maps[s[-1]])
+        
