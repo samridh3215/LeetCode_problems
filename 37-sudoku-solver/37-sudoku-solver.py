365e03fd22378d1087dd0f684e225e9c81ea1b70
Samridh Anand <89855495+samridh3215@users.noreply.github.com>
2022-09-12 18:33:32 +0530

    Time: 1686 ms (5.95%), Space: 14 MB (72.96%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.py
@@ -0,0 +1,30 @@
+class Solution:
+    
+    
+    
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        
+        def isValid(num,row,col,board,N):
+            for i in range(N):
+                if str(num) == board[row][i]: 
+                    return False
+                if str(num) == board[i][col]: 
+                    return False
+                if str(num) == board[3*(row//3) +i//3][3*(col//3) +i%3]: 
+                    return False
+            return True
+        
+        N=len(board)
+        for row in range(N):
+            for col in range(N):
+                if board[row][col]==".":
+                    for num in range(1,10):
+                        if(isValid(num,row,col,board,N)):
+                            board[row][col]=str(num)
+                            if(self.solveSudoku(board)):
+                                return True
+                            else:
+                                board[row][col]="."
+                    return False
+        return True
+    
