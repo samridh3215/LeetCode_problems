23373221cbc1da2b9795866167622fc2013ccfd1
Samridh Anand <89855495+samridh3215@users.noreply.github.com>
2024-01-14 20:20:54 +0530

    Time: 69 ms (99.18%), Space: 18.2 MB (17.81%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,30 +3,30 @@ class Solution:
         if t=="":
             return ""
 
-        tDic = {}
+        tDic = {}  #Create a count record of all characters 
         for i in t:
             tDic[i] = 1 + tDic.get(i, 0)
-        winDic = {}
-        have, need = 0, len(tDic)
-        l = 0
-        ans = [-1, -1]
+        winDic = {} #store charcter and count of sliding window
+        have, need = 0, len(tDic)  # have and need should match i.e a substring is present in s
+        l = 0 #left pointer 
+        ans = [-1, -1]  #to store the ends of left and right pointers
         res = float("infinity")
         for r in range(len(s)):
             c = s[r]
-            winDic[c] = 1 + winDic.get(c, 0)   
-            if c in tDic and tDic[c] == winDic[c]:
-                have+=1
-            while have == need:
+            winDic[c] = 1 + winDic.get(c, 0)     #update window dicttionary
+            if c in tDic and tDic[c] == winDic[c]:  # updating have value if it exists in t
+                have+=1 
+            while have == need:  # to check and update if its minimum length window 
                 if(r-l+1) < res:
                     ans = [l, r]
                     res = r-l+1
-                winDic[s[l]] -= 1
-                if s[l] in tDic and winDic[s[l]] < tDic[s[l]]:
+                winDic[s[l]] -= 1 # remove the the left character in wndow 
+                if s[l] in tDic and winDic[s[l]] < tDic[s[l]]:  # update have value 
                     have-=1
-                l+=1
-        l, r = ans
+                l+=1  # move the left pointer
+        l, r = ans  # deconstruct the array to get left and right pointers of window
         if(res != float("infinity")):
-            return s[l:r+1]
+            return s[l:r+1] 
         else:
             return ""
 
